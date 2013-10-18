7b558e224b2d6321def91e6f71214b498390c465
Oliver Nightingale <oliver.nightingale1@gmail.com>
2013-10-18 16:27:44 +0100

    Add multi page example

---
+++ examples/multi_page.rb
@@ -0,0 +1,25 @@
+require 'led_board'
+
+board = LEDBoard.connect(1)
+
+message_1 = LEDBoard::Page.new("Page 1",
+                                  page: 'A',
+                                  leading: LEDBoard::Leading::SCROLL_LEFT,
+                                  lagging: LEDBoard::Lagging::SCROLL_RIGHT,
+                                  waiting: LEDBoard::Waiting::MEDIUM,
+                                  color: LEDBoard::Color::RED)
+
+message_2 = LEDBoard::Page.new("Page 2",
+                                  page: 'B',
+                                  leading: LEDBoard::Leading::SCROLL_LEFT,
+                                  lagging: LEDBoard::Lagging::SCROLL_RIGHT,
+                                  waiting: LEDBoard::Waiting::MEDIUM,
+                                  color: LEDBoard::Color::GREEN)
+
+schedule = LEDBoard::Schedule.new(%w(A B))
+
+board.send(message_1)
+board.send(message_2)
+board.send(schedule)
+
+board.disconnect
